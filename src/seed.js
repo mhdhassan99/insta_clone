96c69fe098bdf2273d43aa17b700f0994afcf3bf
mhdhassan99 <mhassan.work99@gmail.com>
2021-03-24 16:53:12 -0400

    seed data

--- src/seed.js
+++ src/seed.js
@@ -1,5 +1,5 @@
 export function seedDatabase(firebase) {
-    
+
   const users = [
     {
       userId: 'NvPY9M9MzFTARQ6M816YAzDJxZ72',
@@ -30,3 +30,48 @@ export function seedDatabase(firebase) {
       followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
       dateCreated: Date.now()
     },
+    {
+      userId: '4',
+      username: 'orwell',
+      fullName: 'George Orwell',
+      emailAddress: 'george@orwell.com',
+      following: [],
+      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
+      dateCreated: Date.now()
+    }
+  ];
+
+  for (let k = 0; k < users.length; k++) {
+    firebase.firestore().collection('users').add(users[k]);
+  }
+
+  // eslint-disable-next-line prefer-const
+  for (let i = 1; i <= 5; ++i) {
+    firebase
+      .firestore()
+      .collection('photos')
+      .add({
+        photoId: i,
+        userId: '2',
+        imageSrc: `/images/users/raphael/${i}.jpg`,
+        caption: 'Saint George and the Dragon',
+        likes: [],
+
+        comments: [
+          {
+            displayName: 'dali',
+            comment: 'Love this place, looks like my animal farm!'
+          },
+          {
+            displayName: 'orwell',
+            comment: 'Would you mind if I used this picture?'
+          }
+        ],
+        
+        userLatitude: '40.7128°',
+        userLongitude: '74.0060°',
+        dateCreated: Date.now()
+      });
+
+  }
+}
