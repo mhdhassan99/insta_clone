dfa6efadf514ff73fc34ec76cb585f5879edddc3
mhdhassan99 <mhassan.work99@gmail.com>
2021-03-30 20:19:41 -0400

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Login = lazy(() => import ('./pages/login'));
 
 function App() {
   return (
-    <p>Hello</p>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path='/login' component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ src/pages/login.js
@@ -0,0 +1,4 @@
+
+export default function Login() {
+    return <p>I am login page</p>
+}
