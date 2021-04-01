083f2040da29c8468ad175283b7d64bd1006f6a7
mhdhassan99 <mhassan.work99@gmail.com>
2021-03-31 22:22:24 -0400

    working on login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,19 @@
+import { useState, useContext } from 'react';
+import { useHistory } from "react-router"
+import FirebaseContext from '../context/firebase';
 
 export default function Login() {
+
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext)
+
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleLogin = () => {};
+
     return <p>I am login page</p>
 }
