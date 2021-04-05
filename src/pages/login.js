80152fef4e86e028b7cdf2a9ff7f4de6387efa4c
mhdhassan99 <mhassan.work99@gmail.com>
2021-04-05 19:28:04 -0400

    working on login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -26,7 +26,31 @@ export default function Login() {
             </div>
 
             <div className="flex flex-col w-2/5">
-                <p>I will be the form!</p>
+
+                <h1 className="flex justify-center w-full">
+                    <img src="images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+                </h1>
+
+                {error && <p className="mb-4 text-xs text-red-primary" >{error}</p>}
+
+                <form onSubmit={handleLogin} method="post">
+                    <input 
+                        aria-label="Enter Your Email"
+                        type="text"
+                        placeholder="Email Address"
+                        className="text-sm text-gray-base w-full mr-3 py-3 px-4 h-2 border border-gray-primary rounded mb-2"
+                        onChange={({ target }) => console.log(target.value)}
+                    />
+                    <input 
+                        aria-label="Enter Your Password"
+                        type="password"
+                        placeholder="Password"
+                        className="text-sm text-gray-base w-full mr-3 py-3 px-4 h-2 border border-gray-primary rounded mb-2"
+                        onChange={({ target }) => console.log(target.value)}
+                    />
+
+                </form>
+
             </div>
         </div>
     )
