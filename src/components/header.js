332d21ec9f55613a0f41168182fb06136bfb4644
mhdhassan99 <mhassan.work99@gmail.com>
2021-04-13 21:38:09 -0400

    working on the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,19 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+import UserContext from '../context/user';
+import * as ROUTES from '../constants/routes';
 
 export default function Header() {
     
-    return <p>I am the header</p>
+    const { firebase } = useContext(FirebaseContext);
+    const { user } = useContext(UserContext);
 
+    console.log('user', user)
+
+    return (
+        <header className="h-16 bg-white border-b border-gray-primary mb-8">
+            <p>hello</p>
+        </header>
+    );
 }
--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 
 export default function useAuthListener() {
-    
+
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
     const { firebase } = useContext(FirebaseContext);
 
@@ -14,9 +14,15 @@ export default function useAuthListener() {
                 localStorage.setItem('authUser', JSON.stringify(authUser));
                 setUser(authUser)
             }else {
+            // if we don't have a user clear the local storage 
                 localStorage.removeItem('authUser');
                 setUser(null);
             }
         })
-    }, [])
+
+        return () => listener()
+
+    }, [firebase])
+
+    return { user };
 }
