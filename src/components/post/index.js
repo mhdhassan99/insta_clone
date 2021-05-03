48125f69d31afb2b35537b34456878f65ad95d7e
mhdhassan99 <mhassan.work99@gmail.com>
2021-05-03 19:52:06 -0400

    workig to create time line

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,17 +1,30 @@
 import { useRef } from 'react';
 import PropTypes from 'prop-types';
+import Header from './header'
 
 export default function Post({ content }) {
-    return <p>I am a post</p>
+    //components 
+    // header, image, action(like and comment icons), footer, comments
+
+    // console.log('content', content)
+
+    return (
+        <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+            <Header username={content.username} />
+        </div>
+    )
+
 }
 
 Post.propTypes = {
     content: PropTypes.shape({
         username: PropTypes.string.isRequired,
         imageSrc: PropTypes.string.isRequired,
-        username: PropTypes.string.isRequired,
-        username: PropTypes.string.isRequired,
-        username: PropTypes.string.isRequired,
-        username: PropTypes.string.isRequired
+        caption: PropTypes.string.isRequired,
+        docId: PropTypes.string.isRequired,
+        userLikedPhoto: PropTypes.bool.isRequired,
+        likes: PropTypes.array.isRequired,
+        comments: PropTypes.array.isRequired,
+        dateCreated: PropTypes.number.isRequired
     })
 }
