0df7e1ecd5566861bbdbd86426195a886b63b9b9
mhdhassan99 <mhassan.work99@gmail.com>
2021-05-04 19:20:20 -0400

    working to create actions like likes and comments

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -16,6 +16,45 @@ export default function Actions({dicId, totalLikes, likedPhoto, handelFocus }) {
 
     const handleToggleLiked = async () => {
         setToggleLiked((toggleLiked) => !toggleLiked);
+
+        await firebase
+            .firestore()
+            .collection('photos')
+            .doc(docId)
+            .update({
+                likes: toggleLiked ? FieldValue.arrayRemove(userId) : FieldValue.arrayUnion(userId)
+            });
+
+        setLikes((likes) => (toggleLiked ? likes - 1 : likes + 1));
     };
-    
+
+    return (
+        <>
+            <div className="flex justify-between p-4">
+                <div className="flex">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        tabIndex={0}
+                    >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                    </svg>
+                </div>
+            </div>
+        </>
+    )
+};
+
+Actions.prototype = {
+    docId: PropTypes.string.isRequired,
+    totalLikes: PropTypes.number.isRequired,
+    likedPhoto: PropTypes.bool.isRequired,
+    handelFocus: PropTypes.func.isRequired
 };
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
 import Image from './image';
+import Actions from './actions';
 
 export default function Post({ content }) {
     //components 
@@ -13,6 +14,11 @@ export default function Post({ content }) {
         <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
             <Header username={content.username} />
             <Image src={content.imageSrc} caption={content.caption} />
+            <Actions 
+                docId={content.docId}
+                totalLikes={content.likes.length}
+                LikedPhoto={content.userLikedPhoto}
+            />
         </div>
     )
 
